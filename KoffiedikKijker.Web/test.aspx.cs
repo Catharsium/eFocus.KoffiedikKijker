eaa136eb0d8ca671bd5960b068c6c57c00d9479b
Thierry Brachthuizer <thierry.brachthuizer@efocus.nl>
2016-01-15 10:05:32 +0100

    Project Oxford tests

---
+++ KoffiedikKijker.Web/test.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.UI;
+using Microsoft.ProjectOxford.Vision;
+
+namespace KoffiedikKijker.Web
+{
+    public partial class test : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            iets();
+        }
+
+
+        private void iets()
+        {
+            RunTest();
+        }
+
+        private async void RunTest()
+        {
+            var client = new VisionServiceClient("bae2f176b113415f81bdca26eccab2e9");
+            var result = await client.AnalyzeImageAsync("http://dailycoffeenews.com/wp-content/uploads/2013/02/empty-coffee-mug-300x200.jpeg");
+        }
+    }
+}
